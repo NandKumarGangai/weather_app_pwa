eff2f3e139e11d8f7ae25bd7ec1197ca28c50271
nandkumargangai <nandkumargangai1@outlook.com>
2022-01-01 22:38:00 +0530

    refactors

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,7 @@
 import Axios from "axios"
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY='<key>';
+const API_KEY = process.env.API_KEY;
 
 export const fetchWeatherData = async (query) => {
     const { data } = await Axios.get(URL, {
